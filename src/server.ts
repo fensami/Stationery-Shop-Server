9ac565674432c592d07574f50bb298a00daddaf5
Asad <fensami2323@gmail.com>
2024-11-22 11:33:56 +0600

    change the rungging port

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ import app from "./app";
 async function main() {
     try {
 
-        app.listen(3000, () => {
+        app.listen(5000, () => {
             console.log(`Example app listening on port 3000`);
 
         })
