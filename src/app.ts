06915088be7cd0880d64cb8db91a2125aac1daa7
Asad <fensami2323@gmail.com>
2025-03-11 13:07:46 +0600

    server open

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(cors());
 
-app.use(cors({ origin: 'https://stationery-shop-client-seven.vercel.app', credentials: true }));
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 // app.use(cors({ origin: '*', credentials: true }));
 //Applications Routes 
 app.use("/api", router);
@@ -18,7 +18,7 @@ app.use("/api", router);
 const test = async (req: Request, res: Response) => {
     res.send({
         status: 200,
-        message: "hello!! welcome to Stationery shop Server"
+        message: "hello!! welcome to second hand Shop Server"
     })
 }
 
